0a5e85f51d51520efe28fd00fb2bd118726c76d9
Bharath470 <96712361+Bharath470@users.noreply.github.com>
2025-04-27 17:18:32 +0530

    Project 5 done

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,17 @@
-console.log('Project 5');
+const insert = document.querySelector(".key")
+window.addEventListener('keydown',function(e){
+  insert.innerHTML = `
+  <table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key ===' '? 'space' :e.key }</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+  `
+})
